dcd6bd701f28eccb0de8a0a20ffce1401ce1120f
thehoangbka1997 <49914551+thehoangbka1997@users.noreply.github.com>
2022-10-22 15:36:30 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Xin chào ! 🖐🖐🖐',
+    introDesc: `T có chuyện muốn hỏi. B trả lời thật lòng nhé!`,
+    btnIntro: 'OK',
+    title: 'B thích t đúng không？ 😙',
+    desc: 'Không trở lời hoặc thoát ra chứng tỏ là thích nhé :> ',
+    btnYes: 'Siêu siêu thích 💖💖💖',
+    btnNo: '💔 Không ! 💔 ',
+    question:'Tại sao thích t vậy 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gửi ! 💌💌',
+    reply: 'Vì Hoàng quá đẹp trai 😊😊',
+    mess: 'Ra vậy. Cảm ơn nhé 🥰',
+    messDesc: '💬💬 Nhắn tin t nhé. 💬💬',
+    btnAccept: 'Oki 💖',
+    messLink: 'http://m.me/thehoang.nguyen.37'
+}
