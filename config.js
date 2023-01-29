232eefb305778777d04d7b79b82807468be12136
thehoangbka1997 <49914551+thehoangbka1997@users.noreply.github.com>
2023-01-29 22:04:48 +0900

    a

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Xin chào e chút! 🖐🖐🖐',
-    introDesc: `E chút cho mình hỏi chuyện ni nha!`,
-    btnIntro: 'OK',
+    introTitle: '🖐🖐 Xin chào e chút! 🖐🖐',
+    introDesc: `E chút ơi cho mình hỏi chuyện ni nha!`,
+    btnIntro: 'Dạ',
     title: 'E chút có đồng ý lấy a không？ 😙',
     desc: 'Không trở lời hoặc thoát ra là đồng ý nhé :> ',
-    btnYes: 'Dạ e đồng  💖💖💖',
+    btnYes: 'Dạ e đồng ạ 💖💖💖',
     btnNo: '💔 Không ! 💔 ',
-    question:'E chút đồng ý làm vợ a rồi cùng a xây dựng 1 gia đình siêu hạnh phúc nha 😊😊',
+    question:'Vậy là e chút đồng ý làm vợ a rồi cùng a xây dựng 1 gia đình siêu hạnh phúc nha 😊😊',
     reasonPlaceholder: 'Câu trả lời',
     btnReply: 'Gửi ! 💌💌',
     reply: 'Dạ a Hoàng  😊😊',
     mess: 'Cảm ơn e chút  🥰',
     messDesc: '💬💬 Nhắn tin cho a đi. 💬💬',
-    btnAccept: 'Oki 💖',
+    btnAccept: 'Dạaa 💖',
     messLink: 'http://m.me/thehoang.nguyen.37'
 }
